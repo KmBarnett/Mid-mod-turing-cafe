2c525fc3a602ba5ec119d66522c177942315c7cb
kmbarnett <kmbarnett94@gmail.com>
2020-04-02 11:54:11 -0600

    Add integration test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App.js';
+import { getReservations } from '../APIControlor.js'
+jest.mock('../APIControlor.js')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+function renderForm() {
+  return render(<App />)
+}
+describe('App', () => {
+  getReservations.mockResolvedValueOnce([])
+
+  it('should makeReservation reservation', () => {
+  const { getByText, getByPlaceholderText, getByTestId  } = renderForm()
+  const name = getByPlaceholderText('Name')
+  const date = getByPlaceholderText('mm/dd')
+  const time = getByPlaceholderText('00:00')
+  const number = getByPlaceholderText('Party Size')
+  const reserveBtn = getByText('Reserve')
+
+
+  fireEvent.change(name, { target: { value: 'John' } })
+  fireEvent.change(date, { target: { value: '04/01' } })
+  fireEvent.change(time, { target: { value: '7:30' } })
+  fireEvent.change(number, { target: { value: '3' } })
+  fireEvent.click(reserveBtn)
+
+  const nameDisplay = getByTestId('guest')
+  const dateDisplay = getByTestId('date')
+  const timeDisplay = getByTestId('time')
+  const partyDisplay = getByTestId('party')
+
+  expect(nameDisplay).toBeInTheDocument()
+  expect(dateDisplay).toBeInTheDocument()
+  expect(timeDisplay).toBeInTheDocument()
+  expect(partyDisplay).toBeInTheDocument()
+
+
+  })
+
+})
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,11 +4,11 @@ import './Card.css';
 function Card({ id, name, date , time, number }) {
     return (
       <section id={id} className="card">
-        <h4 className='guest'>{name}</h4>
+        <h4 data-testid='guest' className='guest'>{name}</h4>
         <ul className='info'>
-          <li><span className='label'>Date:</span> {date}</li>
-          <li><span className='label'>Time:</span> {time}</li>
-          <li><span className='label'>Party:</span> {number}</li>
+          <li data-testid='date'><span className='label'>Date:</span> {date}</li>
+          <li data-testid='time'><span className='label'>Time:</span> {time}</li>
+          <li data-testid='party'><span className='label'>Party:</span> {number}</li>
         </ul>
       </section>
     )
