9a8b93f3c3a7f13f54a064c37bb760de06b17804
kmbarnett <kmbarnett94@gmail.com>
2020-04-02 09:54:32 -0600

    Add reservation data

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -3,11 +3,11 @@ import './Reservartions.css';
 import Card from '../Card/Card.js'
 
 
-function Reservations() {
+function Reservations({ reservations }) {
     return (
       <section className="Reservations">
         <h2>Reservations:</h2>
-
+        {reservations.map(reservation => <Card reservation={reservation} />)}
       </section>
     )
 }
