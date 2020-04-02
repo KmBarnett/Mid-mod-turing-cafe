981f45e8bb23d3e1d79305e2cf8b989bfedfe8cf
kmbarnett <kmbarnett94@gmail.com>
2020-04-02 10:08:03 -0600

    Add Card display

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './Card.css';
 
-function Card() {
+function Card({ id, name, date , time, number }) {
     return (
-      <section className="Card">
-        <h4>Card</h4>
+      <section id={id} className="card">
+        <h4 className='guest'>{name}</h4>
+        <ul className='info'>
+          <li>Date: {date}</li>
+          <li>Time: {time}</li>
+          <li>Party: {number}</li>
+        </ul>
       </section>
     )
 }
