309401fba76a4dd27fa24fff5396975b98b10503
kmbarnett <kmbarnett94@gmail.com>
2020-04-02 11:44:51 -0600

    Add Testing to two componants and testing form filling out

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,13 +3,13 @@ import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Form from './Form.js';
 
-function renderLoginForm() {
+function renderForm() {
   return render(<Form makeReservation = {jest.fn()} />)
 }
-describe('LoginForm', () => {
+describe('Form', () => {
 
   it('Should Have its form', () => {
-    const { getByText } = renderLoginForm()
+    const { getByText } = renderForm()
 
     const nameInput = getByText('Name:')
     const dateInput = getByText('Date:')
@@ -22,6 +22,24 @@ describe('LoginForm', () => {
     expect(timeInput).toBeInTheDocument()
     expect(partyInput).toBeInTheDocument()
     expect(reserveBtn).toBeInTheDocument()
+  })
+
+  it('should update reservation', () => {
+  const { getByPlaceholderText, debug } = renderForm()
+  const name = getByPlaceholderText('Name')
+  const date = getByPlaceholderText('mm/dd')
+  const time = getByPlaceholderText('00:00')
+  const number = getByPlaceholderText('Party Size')
+
+  fireEvent.change(name, { target: { value: 'John' } })
+  fireEvent.change(date, { target: { value: '04/01' } })
+  fireEvent.change(time, { target: { value: '7:30' } })
+  fireEvent.change(number, { target: { value: '3' } })
+
+  expect(name.value).toBe('John')
+  expect(date.value).toBe('04/01')
+  expect(time.value).toBe('7:30')
+  expect(number.value).toBe('3' )
   })
 
 })
